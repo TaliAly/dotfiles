10c0e5ad2ef003744569a9faadef7ea44ac2710b
Tali <allysonnomuraga@gmail.com>
2024-02-22 22:07:07 -0500

    hyprland wm

--- removedots.sh
+++ removedots.sh
@@ -7,19 +7,25 @@ for folder in ${folders[@]}; do
   stow -D $folder
 done
 
-echo "Now, this will delete all the files inside the ~/.config folder."
-echo "Do you want to continue?"
+#################################
+#
+# Remove ~/.config
+#
+#################################
 
-read -p "Continue? [y/n]" -n 1 -r
-echo
-
-if [[ ! $REPLY =~ ^[Yy]$ ]]
-then
-    [[ "$0" = "$BASH_SOURCE" ]] && exit 1 || return 1
-fi
-
-readarray folders < stowlist
-for folder in ${folders[@]}; do
-    echo "Removing ~/.config/$folder"
-  rm -rf ~/.config/$folder
-done
+# echo "Now, this will delete all the files inside the ~/.config folder."
+# echo "Do you want to continue?"
+#
+# read -p "Continue? [y/n]" -n 1 -r
+# echo
+#
+# if [[ ! $REPLY =~ ^[Yy]$ ]]
+# then
+#     [[ "$0" = "$BASH_SOURCE" ]] && exit 1 || return 1
+# fi
+#
+# readarray folders < stowlist
+# for folder in ${folders[@]}; do
+#     echo "Removing ~/.config/$folder"
+#   rm -rf ~/.config/$folder
+# done
